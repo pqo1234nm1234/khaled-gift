102eaad504944991765b2896a0dd769a9762da5c
pqo1234nm1234 <pqo1234nm1234@gmail.com>
2026-06-06 16:01:31 +0300

    Update race.js

--- race.js
+++ race.js
@@ -6,7 +6,7 @@ const supabaseClient = window.supabase.createClient(
     SUPABASE_KEY
 );
 
-const WIN_CHANCE = 0.05;
+const WIN_CHANCE = 0.07;
 
 const startBtn = document.getElementById("startBtn");
 const lobby = document.getElementById("lobby");
